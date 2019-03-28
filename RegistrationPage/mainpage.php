e92837b58f34e790effc69b6017d954bf92b35b8
Varun Mehta <varun.me96@gmail.com>
2019-03-28 07:55:15 +0530

    Updated DBControl

---
+++ RegistrationPage/mainpage.php
@@ -0,0 +1,102 @@
+<?php
+if(!empty($_POST["register-user"])) {
+
+	foreach($_POST as $key=>$value) {
+		if(empty($_POST[$key])) {
+		$error_message = "All Fields are required";
+		break;
+		}
+	}
+	
+	if($_POST['password'] != $_POST['confirm_password']){ 
+	$error_message = 'Passwords should be same<br>'; 
+	}
+
+	
+	if(!isset($error_message)) {
+		if (!filter_var($_POST["userEmail"], FILTER_VALIDATE_EMAIL)) {
+		$error_message = "Invalid Email Address";
+		}
+	}
+
+	
+	if(!isset($error_message)) {
+	if(!isset($_POST["gender"])) {
+	$error_message = " All Fields are required";
+	}
+	}
+
+	
+	if(!isset($error_message)) {
+		if(!isset($_POST["terms"])) {
+		$error_message = "Accept Terms and Conditions to Register";
+		}
+	}
+	if(!isset($error_message)) {
+		require_once("dbcontrol.php");
+		$db_handle = new DBcontrol();
+		$query = "INSERT INTO registered_users (user_name, first_name, last_name, password, email, gender) VALUES
+		('" . $_POST["userName"] . "', '" . $_POST["firstName"] . "', '" . $_POST["lastName"] . "', '" . md5($_POST["password"]) . "', '" . $_POST["userEmail"] . "', '" . $_POST["gender"] . "')";
+		$result = $db_handle->insertQuery($query);
+		if(!empty($result)) {
+			$error_message = "";
+			$success_message = "You have registered successfully!";	
+			unset($_POST);
+		} else {
+			$error_message = "Problem in registration. Try Again!";	
+		}
+	}
+
+
+}
+?>
+<html>
+<head>
+<title>PHP User Registration Form</title>
+</head>
+<body>
+<form name="frmRegistration" method="post" action="">
+<table border="0" width="500" align="center" class="demo-table">
+<?php if(!empty($success_message)) { ?>	
+<div class="success-message"><?php if(isset($success_message)) echo $success_message; ?></div>
+<?php } ?>
+<?php if(!empty($error_message)) { ?>	
+<div class="error-message"><?php if(isset($error_message)) echo $error_message; ?></div>
+<?php } ?>
+<tr>
+<td>User Name</td>
+<td><input type="text" class="demoInputBox" name="userName" value="<?php if(isset($_POST['userName'])) echo $_POST['userName']; ?>"></td>
+</tr>
+<tr>
+<td>First Name</td>
+<td><input type="text" class="demoInputBox" name="firstName" value="<?php if(isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></td>
+</tr>
+<tr>
+<td>Last Name</td>
+<td><input type="text" class="demoInputBox" name="lastName" value="<?php if(isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></td>
+</tr>
+<tr>
+<td>Password</td>
+<td><input type="password" class="demoInputBox" name="password" value=""></td>
+</tr>
+<tr>
+<td>Confirm Password</td>
+<td><input type="password" class="demoInputBox" name="confirm_password" value=""></td>
+</tr>
+<tr>
+<td>Email</td>
+<td><input type="text" class="demoInputBox" name="userEmail" value="<?php if(isset($_POST['userEmail'])) echo $_POST['userEmail']; ?>"></td>
+</tr>
+<tr>
+<td>Gender</td>
+<td><input type="radio" name="gender" value="Male" <?php if(isset($_POST['gender']) && $_POST['gender']=="Male") { ?>checked<?php  } ?>> Male
+<input type="radio" name="gender" value="Female" <?php if(isset($_POST['gender']) && $_POST['gender']=="Female") { ?>checked<?php  } ?>> Female
+</td>
+</tr>
+<tr>
+<td colspan=2>
+<input type="checkbox" name="terms"> I accept Terms and Conditions <input type="submit" name="register-user" value="Register" class="btnRegister"></td>
+</tr>
+</table>
+</form>
+</body></html>
